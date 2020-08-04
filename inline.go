251f65f3812ea81fd835b641cb5475012c8db500
Aspirin2d <aspirin2d@outlook.com>
2020-08-04 14:35:36 +0800

    refactory 3

--- inline.go
+++ inline.go
@@ -1,126 +1,63 @@
 package goink
 
-import (
-	"regexp"
-	"strings"
-)
+// Basic Node of the story
+type InkObj interface {
+	Story() *Story
+	Parent() InkObj
 
-// Inline of the story
+	Next() InkObj
+	SetNext(obj InkObj)
+}
+
+func NewInline(s *Story, input string) error {
+	// Inline
+	i := &Inline{story: s, parent: s.current, raw: input}
+	s.current.SetNext(i)
+	s.current = i
+	return nil
+}
+
+// Inline node of tye story
 type Inline struct {
-	s  *Story // story
-	k  *Knot  // knot
-	p  Node   // prev
-	n  Node   // next
-	ln int    // line number
-
-	comment string
-	tags    []string
-	divert  string
-	text    string
+	story  *Story
+	parent InkObj
+	next   InkObj
 
 	raw string
 }
 
-// Story of the content
-func (i *Inline) Story() *Story {
-	return i.s
+func (i *Inline) Render() string {
+	return i.raw
 }
 
-// LN - line number of the content
-func (i *Inline) LN() int {
-	return i.ln
+func (i *Inline) Parent() InkObj {
+	return i.parent
 }
 
-// Next content
-func (i *Inline) Next() Node {
-	// divert
-	if i.divert != "" {
-		// try to find local stitch first
-		if i.k != nil {
-			if d := i.k.FindStitch(i.divert); d != nil {
-				return d.Next()
+func (i *Inline) SetNext(obj InkObj) {
+	i.next = obj
+}
+
+func (i *Inline) Next() InkObj {
+	if i.next != nil {
+		return i.next
+	}
+
+	obj := i.parent
+
+	for obj != nil {
+		if c, ok := obj.(*Choices); ok {
+			if c.gather != nil {
+				return c.gather
 			}
 		}
-		// find knot
-		// TODO: sub stitch in knot
-		if d := i.s.FindKnot(i.divert); d != nil {
-			return d.Next()
-		}
-		// panic(errors.Errorf("cannot find the knot: %s", i.divert))
-		return nil
+
+		obj = obj.Parent()
 	}
 
-	if i.n != nil {
-		return i.n
-	}
-
-	// if divert is nil, try to find gather
-	p := i.Prev()
-	var gather *Gather
-	for {
-		if g, ok := p.(*Gather); ok {
-			gather = g
-		}
-
-		if c, ok := p.(*Choices); ok && c.gather != nil && c.gather != gather {
-			return c.gather
-		}
-		if _, ok := p.(Prev); ok {
-			p = p.(Prev).Prev()
-		} else {
-			return nil
-		}
-	}
+	return nil
 }
 
-// SetNext content
-func (i *Inline) SetNext(next Node) {
-	i.n = next
-}
-
-// Prev content
-func (i *Inline) Prev() Node {
-	return i.p
-}
-
-var (
-	commentReg = regexp.MustCompile(`(^.*)(\/\/)(.+)$`)
-	tagReg     = regexp.MustCompile(`(^.*)(\#)(.+)$`)
-	divertReg  = regexp.MustCompile(`(^.*)(\-\>)(.+)$`)
-)
-
-// NewInline from input
-func NewInline(input string) *Inline {
-	i := &Inline{raw: input}
-
-	// comment | spaces trimed
-	if res := commentReg.FindStringSubmatch(input); res != nil {
-		input = res[1]
-		i.comment = strings.TrimSpace(res[3])
-	}
-
-	// tags | spaces trimmed
-	res := tagReg.FindStringSubmatch(input)
-	for res != nil {
-		input = res[1]
-		i.tags = append(i.tags, strings.TrimSpace(res[3]))
-		res = tagReg.FindStringSubmatch(input)
-	}
-	if len(i.tags) > 0 {
-		// reverse tag list
-		for j, k := 0, len(i.tags)-1; j < k; j, k = j+1, k-1 {
-			i.tags[j], i.tags[k] = i.tags[k], i.tags[j]
-		}
-	}
-
-	// divert | spaces trimmed
-	if res := divertReg.FindStringSubmatch(input); res != nil {
-		input = res[1]
-		i.divert = strings.TrimSpace(res[3])
-	}
-
-	// text | spaces not trimmed
-	i.text = input
-
-	return i
+func (i *Inline) Story() *Story {
+	return i.story
 }
