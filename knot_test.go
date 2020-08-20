ebb5b816876176e480f4d11ac17ec5ce50ace936
Aspirin2d <aspirin2d@outlook.com>
2020-08-20 13:15:24 +0800

    add more knot and stitch test cases

--- knot_test.go
+++ knot_test.go
@@ -20,9 +20,29 @@ func TestKnotParsing(t *testing.T) {
 	err := story.Parse(input)
 	assert.Nil(t, err)
 
+	assert.Equal(t, "knot_a", story.paths["knot_a"].(*knot).Name())
+	assert.Equal(t, story, story.paths["knot_a"].(*knot).Story())
+
+	assert.Equal(t, "stitch_a", story.paths["knot_a__stitch_a"].(*stitch).Name())
+	assert.Equal(t, story, story.paths["knot_a__stitch_a"].(*stitch).Story())
+
 	ctx := NewContext()
 	_, err = story.Resume(ctx)
 	assert.Nil(t, err)
+
+	input = `
+	-> Knot_A
+	== Knot_A
+	this is knot a -> stitch_a
+	= stitch_a
+	this is stitch a -> end
+	== Knot_A
+	-> end
+	`
+
+	story = Default()
+	err = story.Parse(input)
+	assert.Contains(t, err.Error(), "conflict knot name")
 }
 
 func TestKnotDivert(t *testing.T) {
@@ -42,9 +62,33 @@ func TestKnotDivert(t *testing.T) {
 
 	story := Default()
 	err := story.Parse(input)
-	assert.Nil(t, err)
-
 	ctx := NewContext()
+
 	_, err = story.Resume(ctx)
 	assert.Nil(t, err)
+}
+
+func TestStitchParsing(t *testing.T) {
+	input := `
+	-> stitch_a
+	= stitch_a
+	-> end
+	`
+
+	story := Default()
+	err := story.Parse(input)
+	assert.Contains(t, err.Error(), "can not find the knot")
+
+	input = `
+	-> knot_a.stitch_a
+	== knot_a
+	hello
+	= stitch_a
+	-> end
+	= stitch_a
+	-> end
+	`
+	story = Default()
+	err = story.Parse(input)
+	assert.Contains(t, err.Error(), "conflict stitch")
 }
